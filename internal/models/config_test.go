e4a41367292d6d62e982065efc6e24505893c63e
zazamaza <safesafy772@gmail.com>
2025-07-31 17:03:10 +0200

    Update config_test.go

--- internal/models/config_test.go
+++ internal/models/config_test.go
@@ -11,17 +11,9 @@ func TestNewYOLOConfiguration(t *testing.T) {
 		InputName:   "images",
 		OutputName:  "output0",
 		InputShape:  []int64{1, 3, 640, 640},
-		OutputShape: []int64{1, 84, 8400},
+		OutputShape: []int64{1, 7, 8400},
 		Classes: []string{
-			"person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat",
-			"traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse",
-			"sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie",
-			"suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove",
-			"skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon",
-			"bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut",
-			"cake", "chair", "couch", "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse",
-			"remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book",
-			"clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush",
+			"Head", "Enemy", "Flashed"
 		},
 		Version: YOLOv11,
 	}
