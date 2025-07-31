5c82c5a0ddb51a2b2302226adf0cabf0492502f9
zazamaza <safesafy772@gmail.com>
2025-07-31 17:04:24 +0200

    Update config.go

--- internal/models/config.go
+++ internal/models/config.go
@@ -27,15 +27,7 @@ func NewYOLOConfiguration() YOLOConfiguration {
 	configuration.InputShape = []int64{1, 3, 640, 640}
 	configuration.OutputShape = []int64{1, 84, 8400}
 	configuration.Classes = []string{
-		"person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat",
-		"traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse",
-		"sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie",
-		"suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove",
-		"skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon",
-		"bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut",
-		"cake", "chair", "couch", "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse",
-		"remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book",
-		"clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush",
+		"Head", "Enemy", "Flashed"
 	}
 	configuration.Version = YOLOv11
 
